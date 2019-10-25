da608c665b286d34614c33917c538c60617ea076
Shubham Kar <shubhamx.kar@intel.com>
2019-10-25 12:17:01 +0530

    tools: Added detached mode option in mqtt publisher
    Change:
    1. Added -d option to run the publisher in detached mode for CITest
    
    Signed-off-by: Shubham Kar <shubhamx.kar@intel.com>

--- mqtt-temp-sensor/publisher.sh
+++ mqtt-temp-sensor/publisher.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "*****Running publisher container*****"
+if [ "$1" = "detached_mode" ]; then
+docker run --rm -itd --net host --name publisher publisher
+else
 docker run --rm -it --net host --name publisher publisher
+fi
